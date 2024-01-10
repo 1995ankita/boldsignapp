6e36a3b390f9188b5500408bcc8ee853ee6874a0
DharaPatelB <111dhara@gmail.com>
2024-01-10 11:23:15 +0530

    cc completed

--- routes/web.php
+++ routes/web.php
@@ -15,29 +15,19 @@ use App\Http\Controllers\TemplateController;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
 
 Route::get('/', function () {
     return view('form');
 });
 
-//Route::get('/createTemplate', [TemplateController::class, 'createTemplate'])->name('createTemplate');
 
 Route::get('/showMailForm', [TemplateController::class, 'showMailForm']);
 Route::post('/send-mail', [TemplateController::class, 'sendMail'])->name('sendMail');;
-
+Route::post('/sendRemind', [TemplateController::class, 'sendRemind']);
+Route::get('/createIdentity', [TemplateController::class, 'createIdentity']);
 Route::get('/list', [TemplateController::class, 'list']);
 Route::get('/download-pdf', [TemplateController::class, 'downloadPdf'])->name('download-pdf');
 Route::get('/download-audittrail', [TemplateController::class, 'downloadAudittrail'])->name('download-audittrail');
 
 Route::get('/authorize',  [AuthController::class, 'redirectToAuthorization'])->name('auth.authorize');
 Route::get('/callback', [AuthController::class, 'getAccessToken'])->name('auth.callback');
-
-Route::post('/sendRemind', [TemplateController::class, 'sendRemind']);
-
-Route::get('/createIdentity', [TemplateController::class, 'createIdentity']);
-
-// // Define the route pointing to the sendMail function in the TemplateController
-// Route::get('/send-mail', [TemplateController::class, 'sendMail'])->name('sendMail');
